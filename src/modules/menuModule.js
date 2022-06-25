75710e747774c525009e2577195ce4c0e7eb59c0
Luka <milojevic.luka2002@gmail.com>
2022-06-25 14:34:45 +0200

    Created menu

--- src/modules/menuModule.js
+++ src/modules/menuModule.js
@@ -1,5 +1,109 @@
 import { emptyContent } from "./emptyContent";
+import allaNorma from "../img/alla-norma.jpeg";
+import bolognese from "../img/bolognese.jpeg";
+import carbonara from "../img/carbonara.jpeg";
+import fettuccine from "../img/fettuccine.jpeg";
+import vongole from "../img/vongole.jpeg";
+import macaroniCheese from "../img/macaroni-cheese.jpeg";
+
 export default function populateMenu() {
   const content = document.querySelector("#content");
   emptyContent();
+
+  // Our recommendation
+  const ourRecommendationSection = document.createElement("section");
+  ourRecommendationSection.classList.add("our-recommendation");
+  const ourRecommendationContainer = document.createElement("div");
+  ourRecommendationContainer.classList.add("container");
+  const ourRecommnedationTitle = document.createElement("h2");
+  ourRecommnedationTitle.textContent = "Our recommendation";
+  ourRecommnedationTitle.classList.add("our-recommendation-title");
+  ourRecommendationContainer.appendChild(ourRecommnedationTitle);
+  const recommendationPastaWrapper = document.createElement("div");
+  recommendationPastaWrapper.classList.add("recommended-pasta");
+  recommendationPastaWrapper.appendChild(
+    dishCreator(
+      "Fettuccine Alfredo",
+      "Also known as fettuccine al burro (fettuccine withbutter), fettuccine alfredo is a simple pasta dishconsisting of fettuccine (flat pasta ribbons) tossed withparmesan cheese and butter. The process emulsifiesthe ingredients for a rich, silky finish.",
+      fettuccine
+    )
+  );
+  recommendationPastaWrapper.appendChild(
+    dishCreator(
+      "Carbonara",
+      "This dish combining a pasta variety such as penne orspaghetti with a cream-based sauce with ham orpancetta. The traditional Roman carbonara saucecontains eggs, a hard cheese such as pecorino orparmesan, cured pork, and black pepper",
+      carbonara
+    )
+  );
+  ourRecommendationContainer.appendChild(recommendationPastaWrapper);
+  ourRecommendationSection.appendChild(ourRecommendationContainer);
+
+  const menuSection = document.createElement("section");
+  menuSection.classList.add("menu");
+  const menuContainer = document.createElement("div");
+  menuContainer.classList.add("container");
+  const menuTitle = document.createElement("h2");
+  menuTitle.classList.add("menu-title");
+  menuTitle.textContent = "Rest of our menu";
+  menuContainer.append(menuTitle);
+
+  const menuWrapper = document.createElement("div");
+  menuWrapper.classList.add("menu-wrapper");
+  menuWrapper.appendChild(
+    dishCreator(
+      "Pasta alla Norma",
+      "The dish consists of a pasta variety such as ziti orrigatoni cooked with aubergine, tomatoes, herbs, spices,capers and cheese.",
+      allaNorma
+    )
+  );
+  menuWrapper.appendChild(
+    dishCreator(
+      "Spaghetti alle Vongole",
+      "The dish can be cooked ‘in bianco’ - with garlic, oil,herbs and white wine, or ‘in rosso’ - with garlic, oil,herbs and tomatoes",
+      vongole
+    )
+  );
+  menuWrapper.appendChild(
+    dishCreator(
+      "Macaroni Cheese",
+      "The dish consists of macaroni pasta, baked in a cheesybechamel sauce in the oven. It is cooked with cheedarcheese and garnished with parsely.",
+      macaroniCheese
+    )
+  );
+  menuWrapper.appendChild(
+    dishCreator(
+      "Spaghetti Bolognese",
+      "Spaghetti bolognaise is a pasta dish consisting ofspaghetti and a sauce made of minced beef,tomatoes, onion, bacon, red wine and herbs.",
+      bolognese
+    )
+  );
+  menuContainer.appendChild(menuWrapper);
+  menuSection.appendChild(menuContainer);
+
+  content.appendChild(ourRecommendationSection);
+  content.appendChild(menuSection);
+}
+
+function dishCreator(dishName, dishDescription, dishImg) {
+  const dish = document.createElement("div");
+  dish.classList.add("dish-wrapper");
+  const dishTitle = document.createElement("h3");
+  dishTitle.classList.add("dish-title");
+  const dishPara = document.createElement("p");
+  dishPara.classList.add("dish-para");
+  const img = document.createElement("img");
+  const imgWrap = document.createElement("div");
+  imgWrap.classList.add("img-wrap");
+  imgWrap.appendChild(img);
+  img.classList.add("dish-image");
+
+  dishTitle.textContent = dishName;
+  dishPara.textContent = dishDescription;
+  img.src = dishImg;
+
+  dish.appendChild(dishTitle);
+  dish.appendChild(dishPara);
+  dish.appendChild(imgWrap);
+
+  return dish;
 }
